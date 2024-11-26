396e375cc0e94a69597673e3937271143dd6598f
Ajay Ambule <ajayambule2003@gmail.com>
2024-11-27 01:01:38 +0530

    src updated

--- src/main/java/soft/transport/management/config/SpringSecurityConfig.java
+++ src/main/java/soft/transport/management/config/SpringSecurityConfig.java
@@ -16,7 +16,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
-@Configuration
+@Configuration 
 @EnableWebSecurity
 public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
 
