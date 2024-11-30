8a386140ea2e54409b4eccde53e23a9a62d76b81
Ajay Ambule <ajayambule2003@gmail.com>
2024-11-30 15:14:06 +0530

    SpringSecurityCongif file updated:

--- src/main/java/soft/transport/management/config/SpringSecurityConfig.java
+++ src/main/java/soft/transport/management/config/SpringSecurityConfig.java
@@ -12,16 +12,16 @@ import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder; 
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration 
 @EnableWebSecurity
-public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
+public class SpringSecurityConfig extends WebSecurityConfigurerAdapter { 
 
     /*@Autowired
-    DataSource dataSource; */
+    DataSource dataSource;  */
 
     @Resource
     private UserDetailsService userDetailsService;
