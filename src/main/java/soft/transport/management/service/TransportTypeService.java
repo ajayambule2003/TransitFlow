aea1964c541e269a00e6d8333330b0f4f6cc75c4
Ajay Ambule <ajayambule2003@gmail.com>
2024-11-30 15:18:40 +0530

    TransportTypeService file updated

--- src/main/java/soft/transport/management/service/TransportTypeService.java
+++ src/main/java/soft/transport/management/service/TransportTypeService.java
@@ -7,7 +7,7 @@ import soft.transport.management.model.TransportType;
 public interface TransportTypeService {
 	
 	void add(TransportType transportType);
-	void update(TransportType transportType);
+	void update(TransportType transportType); 
 	void delete(int id);
 	TransportType findById(int id);
 	List<TransportType> showAllTransportType();
