968d6935d64528ed9a8e9adc43848a5c566fbad8
Ajay Ambule <ajayambule2003@gmail.com>
2024-12-09 21:57:14 +0530

    Billing file updated:

--- src/main/java/soft/transport/management/model/Billing.java
+++ src/main/java/soft/transport/management/model/Billing.java
@@ -6,13 +6,13 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
-import javax.persistence.Transient;
+import javax.persistence.Transient; 
 
-import com.fasterxml.jackson.annotation.JsonIdentityInfo;
-import com.fasterxml.jackson.annotation.ObjectIdGenerators;
+import com.fasterxml.jackson.annotation.JsonIdentityInfo; 
+import com.fasterxml.jackson.annotation.ObjectIdGenerators; 
 
 @Entity 
-@JsonIdentityInfo(generator = ObjectIdGenerators.IntSequenceGenerator.class, property = "id")
+@JsonIdentityInfo(generator = ObjectIdGenerators.IntSequenceGenerator.class, property = "id") 
 @Table(name="billing")
 public class Billing {
 
