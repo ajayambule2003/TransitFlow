366a58e1984aadde92447c355bb371b08c7e7df1
Ajay Ambule <ajayambule2003@gmail.com>
2025-01-31 11:05:30 +0530

    Updated

--- src/test/java/soft/transport/management/TransportManagementApplicationTests.java
+++ src/test/java/soft/transport/management/TransportManagementApplicationTests.java
@@ -1,16 +1,15 @@
 package soft.transport.management;
-
+ 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
+@RunWith(SpringRunner.class) 
 @SpringBootTest
-public class TransportManagementApplicationTests {
+public class TransportManagementApplicationTests { 
 
 	@Test
 	public void contextLoads() {
 	}
-
 }
